4204b432037c6a52d02c9aaa37ffac43ce7221f2
osama11osama <osama11osama@users.noreply.github.com>
2020-11-22 13:10:51 +0100

    Create Receiver.py

---
+++ Receiver.py
@@ -0,0 +1,49 @@
+import math
+from utils import *
+from fuzzy_extractor import FuzzyExtractor
+
+
+class Receiver:
+
+    def __init__(self, crs, Sr):
+        self.crs = crs
+        self.Sr = Sr
+
+    def hashReceiver(self):
+        # """
+        # :param self: crs string which is composed of an RSA modulus N = PQ,
+        #             a uniformly random generator g ∈ ZN
+        #             and pairwise distinct primes p1,....pl.
+        # :return: hashed value of receiver h and random r
+        # """
+
+        primes = []
+        tmp = self.crs.split(' ')
+        N = int(tmp[0])
+        g = int(tmp[1])
+        primesT = list(map(int, tmp[2:]))
+        r = getRandom(N)
+        for i in self.Sr:
+            if i in primesT:
+                primes.append(i)
+        product = math.prod(primes)
+        h = g ** (r * product)
+        res = [h, r]
+        return res
+
+    def checkIntersection(self, s, f, r, i, R):
+        primes = []
+        tmp = list(self.Sr)
+        tmp.pop(i)
+
+        tmpP = self.crs.split(' ')
+        primesT = list(map(int, tmpP[2:]))
+
+        for i in tmp:
+            if i in primesT:
+                primes.append(i)
+        product = math.prod(primes)
+        secArg = f ** (r * product)
+        extractor = FuzzyExtractor(s, secArg)
+        if extractor == R:
+            return i
