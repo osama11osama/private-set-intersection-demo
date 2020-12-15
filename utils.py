e6bcf8232bcd11745ac0f58f66745674d390b2e0
osama11osama <osama11osama@users.noreply.github.com>
2020-12-15 17:45:24 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,46 +1,49 @@
 import random
 
+from sympy import *
+
 
 def isPrime(x):
     count = 0
-    for i in range(int(x/2)):
-        if x % (i+1) == 0:
-            count = count+1
+    for i in range(int(x / 2)):
+        if x % (i + 1) == 0:
+            count = count + 1
     return count == 1
 
-def Random_n_Prime(minNumber, n):
-    #genrate primes from minNumber to minNumber**2
-    primes = [i for i in range(minNumber,minNumber**2) if isPrime(i)]
-    #Chose n random prime from primes and return it
+
+# gives list of n primes starting from the K-th prime to L-th
+def Random_n_Prime(k, l):
+    numbers = [i for i in range(k, l + 1)]
     n_primes = []
-    for i in range(n):
-        rand= random.choice(primes)
-        n_primes.append(rand)
-        primes.remove(rand)
+    for i in range(l):
+        rand = random.choice(numbers)
+        n_primes.append(prime(rand))
+        numbers.remove(rand)
 
     return n_primes
- 
+
+
 def Random_generators(n):
-    #genrate primes under n
-    primes = [i for i in range(1,n) if isPrime(i)]
-    #chose one random prim from primes to finde genrator for it and return the genrator
-    n = random.choice(primes)
+    numbers = [i for i in range(1, n + 1)]
+    # chose one random prim from primes to find generator for it and return the generator
+    n = prime(random.choice(numbers))
     s = set(range(1, n))
     results = []
     for a in s:
         g = set()
         for x in s:
-            g.add((a**x) % n)
+            g.add((a ** x) % n)
         if g == s:
             results.append(a)
-    #print(results)
+    # print(results)
     return random.choice(results)
 
-# changes
+
 def getRandom(greatestElement):
     systemRandom = random.SystemRandom()
     randomNumber = systemRandom.randint(1, greatestElement)
     return randomNumber
+
 
 def prf(greatestIndex):
     """
@@ -63,25 +66,23 @@ def prf(greatestIndex):
             size += 1
     return res
 
+
 def getRandomFromList(l):
-	return l[getRandom(len(l))-1]
-
-def generat_crs(N,l):
-	g = generators (N)
-	distinct_Primes= prf(l)
-	crs = str(N)+' '+ str (g)
-	for i in range(0,len(distinct_Primes)):
-		crs = crs + ' ' + str(distinct_Primes[i])
-
-	return crs
+    return l[getRandom(len(l)) - 1]
 
 
-def test_generator (g, number , size):
+def generat_crs(N, l):
+    g = Random_generators(N)
+    distinct_Primes = prf(l)
+    crs = str(N) + ' ' + str(g)
+    for i in range(0, len(distinct_Primes)):
+        crs = crs + ' ' + str(distinct_Primes[i])
+
+    return crs
+
+
+def test_generator(g, number, size):
     l = {0}
     for i in range(size):
-     l.add(g**i %number)
+        l.add(g ** i % number)
     print(l)
-
-print(getRandom(100))
-print(Random_generators(1000))
-print(Random_n_Prime(23,10))
