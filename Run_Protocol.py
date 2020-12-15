0c2c666c8d6ff90e0f01f086498a9f16fd621691
osama11osama <osama11osama@users.noreply.github.com>
2020-12-15 17:45:46 +0100

    Update Run_Protocol.py

--- Run_Protocol.py
+++ Run_Protocol.py
@@ -1,10 +1,27 @@
-import Crypto
-from Crypto.PublicKey import RSA
-from Crypto import Random
+# import Crypto
+# from Crypto.PublicKey import RSA
+# from Crypto import Random
+from Sender import Sender
+from utils import Random_n_Prime, Random_generators
 
-def run_protocol(client_set, server_set):
 
+def run_protocol(Sender_set, Receiver_set):
+    U = max(Sender_set + Receiver_set)
+    primes = Random_n_Prime(1, U)
+    # chose P and Q then Compute N
+    P = 1987
+    Q = 6997
+    N = P * Q
+    g = Random_generators(N)
+    primes_string = ""
+    for i in range(U):
+        primes_string += str(primes[i]) + " "
 
+    crs = str(N) + " " + str(g) + " " + primes_string
+
+    w = 0
+    h = 0
+    sender = Sender(crs, w, h)
     return 1
 
 
