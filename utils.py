d0a8e94f273d43866ccf3ac653eef92a899fcb5f
osama11osama <osama11osama@users.noreply.github.com>
2020-12-21 15:22:29 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,47 @@
 import random
 from sympy import *
+<<<<<<< Updated upstream
 from Crypto.Hash import HMAC, SHA256
+=======
+
+
+def isPrime(x):
+    count = 0
+    for i in range(int(x / 2)):
+        if x % (i + 1) == 0:
+            count = count + 1
+    return count == 1
+
+
+# gives list of n primes starting from the K-th prime to L-th
+def Random_n_Prime(k, l):
+    numbers = [i for i in range(k, l + 1)]
+    n_primes = []
+    for i in range(l):
+        rand = random.choice(numbers)
+        n_primes.append(prime(rand))
+        numbers.remove(rand)
+
+    return n_primes
+
+
+def Random_generators(n):
+    Primes = [i for i in range(1, n + 1) if isPrime(i)]
+    print(Primes)
+    # chose one random prim from primes to find generator for it and return the generator
+    n = (random.choice(Primes))
+    print(n)
+    s = set(range(1, n))
+    results = []
+    for a in s:
+        g = set()
+        for x in s:
+            g.add((a ** x) % n)
+        if g == s:
+            results.append(a)
+    print(results)
+    return random.choice(results)
+>>>>>>> Stashed changes
 
 
 def getRandom(greatestElement):
@@ -35,11 +76,31 @@ def prf(secretKey, elementsSet):
     return res
 
 
+<<<<<<< Updated upstream
 def generate_crs(N, l):
     g = findGenerator(N)
+=======
+def generat_crs(N, l):
+    g = Random_generators(N)
+
+>>>>>>> Stashed changes
     distinct_Primes = prf(l)
+    # or we just have to use l primes
     crs = str(N) + ' ' + str(g)
     for i in range(0, len(distinct_Primes)):
         crs = crs + ' ' + str(distinct_Primes[i])
 
     return crs
+<<<<<<< Updated upstream
+=======
+
+
+def test_generator(g, number, size):
+    l = {0}
+    for i in range(size):
+        l.add(g ** i % number)
+    print(l)
+
+
+test_generator(5,50,50)
+>>>>>>> Stashed changes
