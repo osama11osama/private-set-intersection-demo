13c04857f6f1b6a847fa21edeef17a587d087f3e
Abdullah Malallah <s8abmala@stud.uni-saarland.de>
2020-12-21 10:46:52 +0100

    generators

--- utils.py
+++ utils.py
@@ -3,46 +3,24 @@ import random
 from sympy import *
 
 
-def isPrime(x):
-    count = 0
-    for i in range(int(x / 2)):
-        if x % (i + 1) == 0:
-            count = count + 1
-    return count == 1
-
-
-# gives list of n primes starting from the K-th prime to L-th
-def Random_n_Prime(k, l):
-    numbers = [i for i in range(k, l + 1)]
-    n_primes = []
-    for i in range(l):
-        rand = random.choice(numbers)
-        n_primes.append(prime(rand))
-        numbers.remove(rand)
-
-    return n_primes
-
-
-def Random_generators(n):
-    numbers = [i for i in range(1, n + 1)]
-    # chose one random prim from primes to find generator for it and return the generator
-    n = prime(random.choice(numbers))
-    s = set(range(1, n))
-    results = []
-    for a in s:
-        g = set()
-        for x in s:
-            g.add((a ** x) % n)
-        if g == s:
-            results.append(a)
-    # print(results)
-    return random.choice(results)
-
-
 def getRandom(greatestElement):
     systemRandom = random.SystemRandom()
     randomNumber = systemRandom.randint(1, greatestElement)
     return randomNumber
+
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
+def findGenerator(N):
+    list = []
+    for i in range(1, N):
+        if gcd(N, i) == 1:
+            list.append(i)
+    return random.choice(list)
 
 
 def prf(greatestIndex):
